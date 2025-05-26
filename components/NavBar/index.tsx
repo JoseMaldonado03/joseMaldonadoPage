22f0e2e5383e2f1ef2bf52c84df9518e973abfdb
José Maldonado <muso030397@gmail.com>
2025-05-26 18:34:51 -0300

    media query

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,7 +1,23 @@
-import { type PropsWithChildren } from 'react';
+'use client';
+
+import { type PropsWithChildren, useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 import classes from './styles.module.css';
 
 export default function NavBar({ children }: PropsWithChildren) {
-  return <nav className={classes.navBar}>{children}</nav>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+    <button className={classes.hamburguesa} onClick={() => setMenuOpen(!menuOpen)}>
+      {menuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+    </button>
+    <nav 
+      data-mobile-open={menuOpen}
+      className={classes.navBar}>{children}</nav>
+      </>
+    );
 }
